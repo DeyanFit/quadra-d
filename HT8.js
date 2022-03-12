40c5c6bee33363067bd529e59ebb48d3c5328d44
DeyanFit <deyan.s.atanasov.2019@elsys-bg.org>
2022-03-12 16:40:53 +0200

    strelqne na patroni

--- HT8.js
+++ HT8.js
@@ -46,6 +46,11 @@ class Bullet {
         context.fillStyle = this.colour
         context.fill()
     }
+    update() {
+        this.show()
+        this.x = this.x + this.speed.x
+        this.y = this.y + this.speed.y
+    }
 }
 
 //create p1
@@ -53,15 +58,37 @@ const sizep1 = 50
 const p1x = canvas.width / 2
 const p1y = canvas.height - sizep1 * 2
 const player1 = new Player(p1x, p1y, sizep1, 'orange')
-player1.show()
+
+const bullets = []
+
+//animation bullets
+function animate() {
+    requestAnimationFrame(animate)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    player1.show()
+    bullets.forEach((bullet) => {
+            bullet.update()
+        })
+        //bullet.show()
+        //bullet.update()
+}
 
 //creating bullets
 addEventListener('click', (event) => {
-    //console.log(event)
-    const bullet = new Bullet(
-        event.clientX, event.clientY,
-        10, 'red',
-        null
+    //angle bullets
+    const angle = Math.atan2(event.clientY - (canvas.height - sizep1 * 2),
+        event.clientX - canvas.width / 2)
+
+    const speed = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+
+    bullets.push(
+        new Bullet(
+            canvas.width / 2,
+            canvas.height - sizep1 * 2,
+            10, 'red', speed)
     )
-    bullet.show()
 })
+animate()

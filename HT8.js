0f9e82a2a2756f680ff849de61c36a2bf240260e
DeyanFit <deyan.s.atanasov.2019@elsys-bg.org>
2022-03-12 18:13:10 +0200

    Update HT8.js

--- HT8.js
+++ HT8.js
@@ -53,6 +53,33 @@ class Bullet {
     }
 }
 
+//enemies create 
+class Enemy {
+    constructor(x, y, sizeE, colour, speed) {
+            this.x = x
+            this.y = y
+
+            this.sizeE = sizeE
+            this.colour = colour
+
+            this.speed = speed
+        }
+        //enemy appearance 
+    show() {
+        context.beginPath()
+        context.arc(this.x, this.y,
+            this.sizeE, 0, Math.PI * 2,
+            false)
+        context.fillStyle = this.colour
+        context.fill()
+    }
+    update() {
+        this.show()
+        this.x = this.x + this.speed.x
+        this.y = this.y + this.speed.y
+    }
+}
+
 //create p1
 const sizep1 = 50
 const p1x = canvas.width / 2
@@ -60,6 +87,27 @@ const p1y = canvas.height - sizep1 * 2
 const player1 = new Player(p1x, p1y, sizep1, 'orange')
 
 const bullets = []
+const enemies = []
+
+//spawn enemy
+function EnemiesSpawn() {
+    setInterval(() => {
+        const x = Math.random() * canvas.width
+        const y = -Math.random() * 200
+        const sizeE = 40
+        const colour = 'blue'
+            //enemy angles
+        const angle = Math.atan2((canvas.height - sizep1 * 2) - y,
+            canvas.width / 2 - x)
+
+        const speed = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        enemies.push(new Enemy(x, y, sizeE, colour, speed))
+    }, 3000)
+}
 
 //animation bullets
 function animate() {
@@ -67,15 +115,17 @@ function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height)
     player1.show()
     bullets.forEach((bullet) => {
-            bullet.update()
-        })
-        //bullet.show()
-        //bullet.update()
+        bullet.update()
+    })
+
+    enemies.forEach((enemy) => {
+        enemy.update()
+    })
 }
 
 //creating bullets
 addEventListener('click', (event) => {
-    //angle bullets
+    //angles bullets
     const angle = Math.atan2(event.clientY - (canvas.height - sizep1 * 2),
         event.clientX - canvas.width / 2)
 
@@ -92,3 +142,4 @@ addEventListener('click', (event) => {
     )
 })
 animate()
+EnemiesSpawn()
